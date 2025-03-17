8345d518f61af7e349c2312cae677214d65988e5
Kohinoor <Knoorchauhan@gmail.com>
2025-03-17 11:14:20 -0700

    Update SmartLock.java

--- Lab10/edu/ucalgary/oop/SmartLock.java
+++ Lab10/edu/ucalgary/oop/SmartLock.java
@@ -1,6 +1,26 @@
 // TODO: package, any includes, class declaration, constructor
 //       and the methods performAction and update.
 
+package edu.ucalgary.oop;
+
+public class SmartLock extends SmartDevice<Boolean> {
+
+    public SmartLock() {
+        super();
+        setState(true);
+    }
+
+    @Override
+    public void update(String message) {
+        switch (message) {
+            case "Sleep", "Vacation":
+                setState(true);
+                performAction();
+                break;
+            default:
+                System.out.println("SmartLight: Unknown message - " + message);
+        }
+    }
     public void autoLock(int delayInSeconds) {
         System.out.println("Auto-lock enabled. Door will lock in " + delayInSeconds + " seconds.");
         new Thread(() -> {
@@ -12,5 +32,15 @@
                 e.printStackTrace();
             }
         }).start();
+    }
+
+    private void performAction() {
+        if (getState()) {
+            System.out.print("SmartLock is Locked");
+            System.out.print("Securing the door");
+        } else {
+            System.out.print("SmartLock is Unlocked.");
+            System.out.print("Door is open.");
+        }
     }
 }
